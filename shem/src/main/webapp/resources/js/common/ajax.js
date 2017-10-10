20ceb78d1dcb3a1317117c9f0f29c2318753895a
wclee <urlover@hanmail.net>
2017-10-10 18:38:13 +0900

    아직은 세팅중!

---
+++ shem/src/main/webapp/resources/js/common/ajax.js
@@ -0,0 +1,5 @@
+
+
+function sendAjax(jsonAjax){
+    $.ajax(jsonAjax);
+}
---
+++ shem/src/main/webapp/resources/js/page/board/board_update.js
@@ -0,0 +1,31 @@
+
+$(function () {
+    $("#nUpdateForm").validate({
+        rules: {
+            title: {required: true},
+            content: {required: true}
+        },
+        messages: {
+            title: {
+                required: "제목을 입력해 주십시오."
+            },
+            content: {
+                required: "내용을 입력해 주십시오."
+            }
+        },
+        submitHandler: function (form) {
+            
+            var ajaxData = {
+                url: '/she/bnd/board/save',
+                type: 'post',
+                data: $(form).serialize() ,
+                success: function (data) {
+                    alert(data.message);
+                    if (data.code === "N200")
+                        location.href = "/she/bnd/board/list";
+                }
+            };
+            sendAjax(ajaxData);
+        }
+    });
+});
